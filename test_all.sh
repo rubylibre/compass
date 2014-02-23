d32724b118816ece8d0470bfc39a9e2e3fbbc732
Scott Davis <me@sdavis.info>
2014-02-23 17:36:38 -0500

    all tests pass

--- test_all.sh
+++ test_all.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd core && bundle install --quiet && bundle exec rake test && cd .. &&
-  cd cli && bundle install --quiet && bundle exec rake && cd ..
+cd cli && bundle install --quiet && bundle exec rake && cd .. &&
+cd sprites && bundle install --quiet && bundle exec rake && cd ..
